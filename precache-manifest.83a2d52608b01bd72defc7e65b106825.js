5c0e5bfe9595c07b4cfe3876c412a75d61277a33
bl96 <47901930+bl96@users.noreply.github.com>
2020-05-07 08:41:26 +0200

    build 3

---
+++ precache-manifest.83a2d52608b01bd72defc7e65b106825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e225e69840b0960aa0dda986016e2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a73c58c7b5ba084e89c",
+    "url": "/static/css/main.31e87ee0.chunk.css"
+  },
+  {
+    "revision": "5e4e27cab3cd156a2414",
+    "url": "/static/js/2.8ce78a9c.chunk.js"
+  },
+  {
+    "revision": "d9fd16d9cf55e20dd34292eecefc56c4",
+    "url": "/static/js/2.8ce78a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a73c58c7b5ba084e89c",
+    "url": "/static/js/main.fb5f3195.chunk.js"
+  },
+  {
+    "revision": "209d4aede668cdc189a8",
+    "url": "/static/js/runtime-main.506e8365.js"
+  }
+]);
